59ad2c39fe5ae7ccf454f9c840853fb1a81a9aae
Carla López Lloreda <carla.lpez09@gmail.com>
2021-11-09 21:12:26 -0500

    Quick plots after merged all synoptic dates- it works!

--- Synoptic/Scripts/02-Merging-files.R
+++ Synoptic/Scripts/02-Merging-files.R
@@ -22,15 +22,20 @@ GHG_202011 <- read_csv("202011_GHG_clean.csv")
 GHG_202102 <- read_csv("202102_GHG_clean.csv")
 GHG_202105 <- read_csv("202105_GHG_Clean.csv")
 GHG_202106 <- read_csv("202106_GHG_Clean.csv")
-GHG_202109 <- read_csv("202109_GHG_Clean.csv")
+GHG_202109 <- read_csv("202109_GHG_Clean_new.csv")
 # GHG_202111 <- <- read_csv("202111_GHG_Clean.csv")
 
 # Merge all synoptic dates together
-GHG_all <- rbind(GHG_202011, GHG_202102, GHG_202105)
+GHG_all <- rbind(GHG_202011, GHG_202102, GHG_202105, GHG_202106, GHG_202109)
 
-ggplot(GHG_all, aes(x= Site_ID, y = CO2_uM)) +
+# Some quick plots
+
+ggplot(GHG_all, aes(x= Site, y = CO2_uM)) +
   geom_boxplot()
 
-ggplot(GHG_all, aes(x= Site_ID, y = CH4_uM)) +
+ggplot(GHG_all, aes(x= Site, y = CH4_uM)) +
   geom_boxplot()
+
+ggplot(GHG_all, aes(x = Sample_Date, y = CH4_uM, color = Site)) +
+  geom_point()
 
