9d60fa21181d4f39d3b4668eb0f4a1de5bcb9e3d
Carla López Lloreda <carla.lpez09@gmail.com>
2024-03-12 12:58:55 -0400

    Updating flux script for DMV

--- Fluxes/LGR UGGA/FluxCalR_Duck Pond.R
+++ Fluxes/LGR UGGA/FluxCalR_Duck Pond.R
@@ -1,7 +1,7 @@
-## Script to  FluxCalR for calculating fluxes from UGGA data
+## Script to *try* FluxCalR for calculating fluxes from UGGA data
 ## A Hounshell, 25 Jan 2021
-## Updated by C López Lloreda for Delmarva wetlands projects
-## Last updated 3/12/2024
+
+## Updated by C López Lloreda for Duck Pond fluxes
 
 # Script following: https://github.com/junbinzhao/FluxCalR
 
@@ -26,19 +26,13 @@ wd <- setwd("C:/Users/Carla López Lloreda/Dropbox/Grad school/Research/Delmarva
 # Could probably be more efficient with this read in 
 # Check James's scripts
 
-flux_lgr_1 <- LoadLGR(file ="./gga_2001-12-31_f0045.txt",
+flux_lgr_1 <- LoadLGR(file ="./gga_2001-12-31_f0012.txt",
                       time_format = "mdy_HMS")
 
-flux_lgr_2 <- LoadLGR(file ="./gga_2001-12-31_f0046.txt",
+flux_lgr_2 <- LoadLGR(file ="./gga_2001-12-31_f0023.txt",
                       time_format = "mdy_HMS")
 
-flux_lgr_3 <- LoadLGR(file ="./gga_2001-12-31_f0047.txt",
-                      time_format = "mdy_HMS")
-
-flux_lgr_4 <- LoadLGR(file ="./gga_2001-12-31_f0048.txt",
-                      time_format = "mdy_HMS")
-
-flux_lgr_5 <- LoadLGR(file ="./gga_2001-12-31_f0049.txt",
+flux_lgr_3 <- LoadLGR(file ="./gga_2001-12-31_f0024.txt",
                       time_format = "mdy_HMS")
 
 # Select times when the UGGA was on/off the water
@@ -47,31 +41,32 @@ flux_lgr_5 <- LoadLGR(file ="./gga_2001-12-31_f0049.txt",
 # When finished, click on 'Stop' in the upper left-hand corner and then click 'Stop locator'
 # This generates a list of 'end' times for each peak saved as time_cue_x
 
-# Still need to fix the actual dates and sites on each of these
+time_cue_1 <- SelCue(flux_lgr_1,flux="CH4",cue="End",save=F)%>%
+  mutate(Site = c("Duck Pond"), Date_real = as.Date("2023-04-17"))
 
-time_cue_2 <- SelCue(flux_lgr_2, flux="CO2", cue="End", save=F)%>%
-  mutate(Site = c("TS"), Date_real = as.Date("2022-06-20"))
+time_cue_2 <- SelCue(flux_lgr_2,flux="CH4",cue="End",save=F)%>%
+  mutate(Site = c("Duck Pond"), Date_real = as.Date("2023-05-11"))
 
-time_cue_3 <- SelCue(flux_lgr_3,flux="CO2",cue="End", save=F)%>%
-  mutate(Site = c("TS"), Date_real = as.Date("2022-06-20"))
-
-time_cue_4 <- SelCue(flux_lgr_4,flux="CO2",cue="End",save=F)%>%
-  mutate(Site = c("TS"), Date_real = as.Date("2022-06-13"))
-
-time_cue_5 <- SelCue(flux_lgr_5,flux="CH4",cue="End",save=F)%>%
-  mutate(Site = c("TS"), Date_real = as.Date("2022-06-13"))
-
-time_cue_6 <- SelCue(flux_lgr_6,flux="CH4",cue="End",save=F)%>%
-  mutate(Site = c("TS"), Date_real = as.Date("2022-06-13"))
-
+time_cue_3 <- SelCue(flux_lgr_3,flux="CH4",cue="End",save=F)%>%
+  mutate(Site = c("Duck Pond"), Date_real = as.Date("2023-05-11"))
 
 # Then calculate fluxes
 # We use 10 minute windows
-# Volume and area stay the same (same chamber as the Carey lab is used)
+# Volume and area stay the same (same chamber is used)
 # Need to change other columns that specific site details
 
-Flux_output2 <- FluxCal(data = flux_lgr_2, # Dataframe loaded in
+flux_output1 <- FluxCal(data = flux_lgr_1, # Dataframe loaded in
                         win = 10, # Window length = 4 minutes
+                        vol = 0.020876028*1000, # Volume of trap in liters
+                        area = 0.1451465, # Area of trap in m^2
+                        df_cue = time_cue_1, # End times selected using SelCue
+                        cue_type = "End", # Designate that these times are for the end
+                        ext = 1, # Multiplier for time window to look at data (5 min x 1 = use full 5 min interval)
+                        other = c("Site","Date_real"),
+                        output = FALSE)
+
+flux_output2 <- FluxCal(data = flux_lgr_2, # Dataframe loaded in
+                        win = 10, # Window length
                         vol = 0.020876028*1000, # Volume of trap in liters
                         area = 0.1451465, # Area of trap in m^2
                         df_cue = time_cue_2, # End times selected using SelCue
@@ -80,53 +75,20 @@ Flux_output2 <- FluxCal(data = flux_lgr_2, # Dataframe loaded in
                         other = c("Site","Date_real"),
                         output = FALSE)
 
-Flux_output3 <- FluxCal(data = flux_lgr_3, # Dataframe loaded in
-                        win = 10, # Window length = 4 minutes
+flux_output3 <- FluxCal(data = flux_lgr_2, # Dataframe loaded in
+                        win = 10, # Window length
                         vol = 0.020876028*1000, # Volume of trap in liters
                         area = 0.1451465, # Area of trap in m^2
-                        df_cue = time_cue_3, # End times selected using SelCue
-                        cue_type = "End", # Designate that these times are for the end
-                        ext = 1, # Multiplier for time window to look at data (5 min x 1 = use full 5 min interval)
-                        other = c("Site","Date_real"),
-                        output = FALSE)
-
-Flux_output4 <- FluxCal(data = flux_lgr_4, # Dataframe loaded in
-                        win = 10, # Window length = 4 minutes
-                        vol = 0.020876028*1000, # Volume of trap in liters
-                        area = 0.1451465, # Area of trap in m^2
-                        df_cue = time_cue_4, # End times selected using SelCue
-                        cue_type = "End", # Designate that these times are for the end
-                        ext = 1, # Multiplier for time window to look at data (5 min x 1 = use full 5 min interval)
-                        other = c("Site","Date_real"),
-                        output = FALSE)
-
-Flux_output5 <- FluxCal(data = flux_lgr_5, # Dataframe loaded in
-                        win = 10, # Window length = 4 minutes
-                        vol = 0.020876028*1000, # Volume of trap in liters
-                        area = 0.1451465, # Area of trap in m^2
-                        df_cue = time_cue_5, # End times selected using SelCue
-                        cue_type = "End", # Designate that these times are for the end
-                        ext = 1, # Multiplier for time window to look at data (5 min x 1 = use full 5 min interval)
-                        other = c("Site","Date_real"),
-                        output = FALSE)
-
-Flux_output6 <- FluxCal(data = flux_lgr_6, # Dataframe loaded in
-                        win = 10, # Window length = 4 minutes
-                        vol = 0.020876028*1000, # Volume of trap in liters
-                        area = 0.1451465, # Area of trap in m^2
-                        df_cue = time_cue_6, # End times selected using SelCue
+                        df_cue = time_cue_2, # End times selected using SelCue
                         cue_type = "End", # Designate that these times are for the end
                         ext = 1, # Multiplier for time window to look at data (5 min x 1 = use full 5 min interval)
                         other = c("Site","Date_real"),
                         output = FALSE)
 
 # Combine all flux outputs
-flux_output <- rbind(Flux_output3,
-                     Flux_output4
+flux_output <- rbind(flux_output2,
+                     flux_output3
                      )
-
-detach('package:dplyr')
-library(dplyr)
 
 # Get together for publication to EDI
 flux_co2 <- flux_output %>% 
@@ -140,28 +102,36 @@ flux_ch4 <- flux_output %>%
   select(-Gas) %>%
   mutate(ch4_flux_nmolCm2s = ch4_flux_umolCm2s*1000)
 
-flux_all <- left_join(flux_co2,flux_ch4,by=c("Num","Date","Start","End","Ta","Reservoir","Site","Date_real"))
+flux_all <- left_join(flux_co2,flux_ch4,by=c("Num","Date","Start","End","Ta","Site","Date_real"))
 
 flux_all <- flux_all %>% 
   rename(Rep = Num, Temp_C = Ta) %>% 
   mutate (co2_flux_umolCm2s_flag = 0, ch4_flux_umolCm2s_flag = 0)
 
-# Filter out R2<0.90
+#col_order <- c("Reservoir","Site","Date","Rep","Start_time","End_time","Temp_C","co2_slope_ppmS","co2_R2",
+#               "co2_flux_umolCm2s","ch4_slope_ppmS","ch4_R2","ch4_flux_umolCm2s","co2_flux_umolCm2s_flag",
+#               "ch4_flux_umolCm2s_flag")
 
-# Plot fluxes
-flux_all%>%
-  ggplot(aes(x = as.Date(Date_real), y=ch4_flux_umolCm2s))+
+#flux_all_2 <- flux_all[,col_order]
+flux_all_2 <- flux_all
+
+# Plots to double check data!
+#flux_all_2$Date <- as.POSIXct(strptime(flux_all_2$Date,"%Y-%m-%d", tz="EST"))
+
+flux_all_2%>%
+  ggplot(aes(x = as.Date(Date_real), color = as.factor(Site),y=ch4_flux_umolCm2s))+
   geom_smooth()+
   geom_point()+
   ylab("CH4 flux (µumol/m2/s)")
 
-flux_all%>%
-  ggplot(aes(x = as.Date(Date_real), y=co2_flux_umolCm2s))+
+flux_all_2%>%
+  ggplot(aes(x = as.Date(Date_real), color = as.factor(Site),y=co2_flux_umolCm2s))+
   geom_smooth()+
   geom_point()+
   ylab("CO2 flux (µumol/m2/s)")
 
-flux_all%>%
+flux_all_2%>%
+  filter(Site==1)%>%
   ggplot(aes(x = as.Date(Date_real),y=ch4_flux_umolCm2s))+
   geom_smooth()+
   geom_point()+
@@ -203,4 +173,4 @@ flux_all_2 = flux_all_2%>%
   select(-Date_real)
 
 # Export out fluxes
-# write_csv(flux_all_2,"./2022_season_Flux_Output.csv")
+write_csv(flux_all_2,"./2022_season_Flux_Output.csv")
