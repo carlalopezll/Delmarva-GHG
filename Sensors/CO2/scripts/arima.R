92d3e99d2875544acaa7ceb80c448cd17ac2bd0a
Carla López Lloreda <carla.lpez09@gmail.com>
2025-07-01 15:45:33 -0400

    adding regressors to arimas

--- Sensors/CO2/scripts/arima.R
+++ Sensors/CO2/scripts/arima.R
@@ -14,30 +14,38 @@ library(broom)
 # read in high-frequency data
 sensors_hf <- read_csv("CO2/data/processed data/merged sensors_250412.csv")
 
-# read in daily data
+sensors_hf <- mutate(sensors_hf, date = as.Date(Timestamp_corrected))
+
+# read in daily data w/ metrics
 sensors_daily <- read_csv("CO2/data/processed data/sensors_daily.csv")
 
 #### Daily means ####
 # Suggestion by Cayelan was to use daily means, amplitude range, instead of the high-frequency data
 
-# Looking at a subset of data without gaps
+# using just a subset of data without gaps
 sensors_daily_subset <- sensors_daily %>%
+  filter(date > "2021-09-28" & date < "2021-11-14")
+
+sensors_hf_subset <- sensors_hf %>%
   filter(date > "2021-09-28" & date < "2021-11-14")
 
 # filtering by site and converting to tstible for arima
 ND_daily <- sensors_daily_subset %>%
   filter(Site_ID == "ND") %>%
   as_tsibble(index = date)
-
 DK_daily <- sensors_daily_subset %>%
   filter(Site_ID == "DK" & !is.na(date)) %>%
   as_tsibble(index = date)
-
 TS_daily <- sensors_daily_subset %>%
   filter(Site_ID == "TS" & !is.na(date)) %>%
   as_tsibble(index = date)
 
-# ARIMA models for ND_daily
+
+ND_hf <- sensors_hf_subset %>%
+  filter(Site_ID == "ND") %>%
+  as_tsibble(index = date)
+
+#### ARIMA models for ND daily ####
 
 adf.test(ND_daily$CO2_uatm_mean)
 acf(ND_daily$CO2_uatm_mean)
@@ -48,16 +56,14 @@ pacf(ND_daily$CO2_uatm_mean)
 y_diff = diff(ND_daily$CO2_uatm_mean, differences = 2)
 
 plot(y_diff)
-pacf(y_diff)
-acf(y_diff)
-adf.test(y_diff)  # significant so we're able to prove stationarity
+pacf(y_diff) # determines p
+acf(y_diff) # determines q
+adf.test(y_diff)  # determines d, if significant we're able to confirm stationarity
 
-# Thus, d = 1
+# Thus, d = 2
 # PACF (p) = 3
 # ACF (q) = 2
-
 # Structure: ARIMA(p,d,q)
-# ARIMA(2,1,1)
 
 # One way to do it
 
@@ -68,30 +74,156 @@ adf.test(y_diff)  # significant so we're able to prove stationarity
 # print(arima_1);print(arima_2);print(arima_3)
 
 # Another way to do it from the forecast package
-# Letting the model choose the orders p,d, aND_daily q
+# Letting the model choose the orders p,d, q
 
 arima_m = auto.arima(ND_daily$CO2_uatm_mean)
-# arima_m = auto.arima(ND_daily$CO2_uatm_mean, , max.p = 0, max.q = 1, max.d = 1, seasonal = F)
 checkresiduals(arima_m)
 summary(arima_m)
 
-# Forcing the orders
+# if you wanted to fix max values of p, d, q, use this
+# arima_m = auto.arima(ND_daily$CO2_uatm_mean, , max.p = 0, max.q = 1, max.d = 1, seasonal = F)
+
+# Forcing the orders using Arima() from the forecast package
 # Structure: ARIMA(p,d,q)
 
-arima_test2 <- Arima(ND_daily$CO2_uatm_mean, order = c(3,1,2))
-checkresiduals(arima_test2)
-summary(arima_test2)
+arima_test <- Arima(ND_daily$CO2_uatm_mean, order = c(3,1,2))
+checkresiduals(arima_test)
+summary(arima_test)
 
 
-# Adding the parameters
+# looking at influence of precip on water level
 
-arimax_test <- Arima(
-  ND_daily$CO2_uatm_mean,
-  order = c(0, 1, 0),
-  xreg = ND_daily$wl_mean
+
+# Adding the parameters to the model
+
+str(ND_daily)
+
+# Define the matrix of external regressors
+xreg_vars <- as.matrix(ND_daily[, c("wl_mean", "wl_CV", "delta_wl")])
+
+# Fit ARIMAX model
+arimax_model <- Arima(ND_daily$CO2_uatm_mean, order = c(3,1,2), xreg = xreg_vars)
+
+# Check residuals
+checkresiduals(arimax_model)
+
+# Summary of model
+summary(arimax_model)
+
+autoplot(residuals(arimax_model))
+forecast::autoplot(forecast(arimax_model, xreg = xreg_vars))
+
+
+# Create data frame with observed and fitted values
+plot_df <- data.frame(
+  DATE = ND_daily$date,
+  Observed = ND_daily$CO2_uatm_mean,
+  Fitted = fitted(arima_test)
 )
 
-checkresiduals(arimax_test)
-summary(arimax_test)
+# Plot
+ggplot(plot_df, aes(x = DATE)) +
+  geom_line(aes(y = Observed), color = "black") +
+  geom_line(aes(y = Fitted), color = "blue") +
+  labs(title = "Observed vs Fitted CO₂", y = "CO₂ (µatm)", x = "Date") +
+  theme_minimal() +
+  scale_y_continuous(labels = scales::comma) +
+  theme(legend.position = "none")
+
+
 # Forecasting lol
-forecast(arima_test2, h = 10) %>% autoplot()
+forecast(arima_test, h = 10) %>% autoplot()
+
+
+
+
+
+#### ARIMA models for ND high-frequency ####
+
+#### Daily means ####
+# Suggestion by Cayelan was to use daily means, amplitude range, instead of the high-frequency data
+
+# using just a subset of data without gaps
+sensors_hf_subset <- sensors_hf %>%
+  filter(date > "2021-09-28" & date < "2021-11-14")
+
+# filtering by site and converting to tstible for arima
+ND_daily <- sensors_hf_subset %>%
+  filter(Site_ID == "ND") %>%
+  as_tsibble(index = date)
+
+DK_daily <- sensors_hf_subset %>%
+  filter(Site_ID == "DK" & !is.na(date)) %>%
+  as_tsibble(index = date)
+
+TS_daily <- sensors_hf_subset %>%
+  filter(Site_ID == "TS" & !is.na(date)) %>%
+  as_tsibble(index = date)
+
+#### ARIMA models for ND daily ####
+
+adf.test(ND_daily$CO2_uatm_mean)
+acf(ND_daily$CO2_uatm_mean)
+pacf(ND_daily$CO2_uatm_mean)
+
+# Compute difference until adf.test is significant
+
+y_diff = diff(ND_daily$CO2_uatm_mean, differences = 2)
+
+plot(y_diff)
+pacf(y_diff) # determines p
+acf(y_diff) # determines q
+adf.test(y_diff)  # determines d, if significant we're able to confirm stationarity
+
+# Thus, d = 2
+# PACF (p) = 3
+# ACF (q) = 2
+# Structure: ARIMA(p,d,q)
+
+# One way to do it
+
+# arima_1=arima(ND_daily$CO2_uatm_mean, order=c(1,0,2))
+# arima_2=arima(ND_daily$CO2_uatm_mean, order=c(0,0,2))
+# arima_3=arima(ND_daily$CO2_uatm_mean, order=c(1,0,0))
+# 
+# print(arima_1);print(arima_2);print(arima_3)
+
+# Another way to do it from the forecast package
+# Letting the model choose the orders p,d, q
+
+arima_m = auto.arima(ND_daily$CO2_uatm_mean)
+checkresiduals(arima_m)
+summary(arima_m)
+
+# if you wanted to fix max values of p, d, q, use this
+# arima_m = auto.arima(ND_daily$CO2_uatm_mean, , max.p = 0, max.q = 1, max.d = 1, seasonal = F)
+
+# Forcing the orders using Arima() from the forecast package
+# Structure: ARIMA(p,d,q)
+
+arima_test <- Arima(ND_daily$CO2_uatm_mean, order = c(3,1,2))
+checkresiduals(arima_test)
+summary(arima_test)
+
+# Adding the parameters to the model
+
+str(ND_daily)
+
+# Define the matrix of external regressors
+xreg_vars <- as.matrix(ND_daily[, c("wl_mean", "wl_CV", "delta_wl")])
+
+# Fit ARIMAX model
+arimax_model <- Arima(ND_daily$CO2_uatm_mean, order = c(3,1,2), xreg = xreg_vars)
+
+# Check residuals
+checkresiduals(arimax_model)
+
+# Summary of model
+summary(arimax_model)
+
+autoplot(residuals(arimax_model))
+forecast::autoplot(forecast(arimax_model, xreg = xreg_vars))
+
+
+# Forecasting lol
+forecast(arima_test, h = 10) %>% autoplot()
